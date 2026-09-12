34b295e4cbfd2ea42c4602be9b52ae154ede0b68
Ubuntu <ubuntu@ip-172-31-26-253.ap-south-1.compute.internal>
2026-09-12 10:48:41 +0000

    Update application message

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from my first CI/CD project!");
+        System.out.println("Hello from jenkins CI/CD!");
     }
 }
