e9d446757428d541d8b94d4c0151030b55e64eae
Ubuntu <ubuntu@ip-172-31-26-253.ap-south-1.compute.internal>
2026-09-14 14:41:39 +0000

    With version control

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins pipeline!");
+        System.out.println("Hello from Jenkins pipeline - Build 11!");
     }
 }
